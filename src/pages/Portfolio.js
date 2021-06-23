0a14ed60b0863896cf9d85fb35a38a4c065473a9
piet <42157469+camper0008@users.noreply.github.com>
2021-06-23 14:14:48 +0200

    updated portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,12 +1,7 @@
 
 import messangerImage from '../assets/images/messanger.jpg';
 import footerIcon from '../assets/images/icons/portfolio-blue.svg';
-import splashImage from '../assets/images/firmacorp_splash.png';
-import productPageImage from '../assets/images/firmacorp_productpage.png';
-
-// import productListImage from '../assets/images/firmacorp_productlist.png';
-// <p>Du kan også spille (mod digselv) <a href={ticTacToeGameFile}>her</a>.</p>
-
+import firmacorpImage from '../assets/images/firmacorp_animated.png';
 
 function Portfolio() {
     return (
@@ -33,8 +28,8 @@ function Portfolio() {
             </pre>
 
           <h2>MessAnger</h2>
-            <p>Som et skoleprojekt lavede jeg sammen med min klassekammerat <a href="https://simonfj20.site" target="_blank" rel="noreferrer">Simon From Jakobsen</a> en webapp hvor man kunne skabe diskussionsrumme og sende beskeder til hinanden, samt med et registerings- og loginssystem.</p>
-            <p>Alt frontend er skabt af mig inkl. programmering i HTML, CSS og TypeScript, og alt backendarbejdet er lavet af <a href="https://simonfj20.site" target="_blank" rel="noreferrer">Simon From Jakobsen</a>.</p>
+            <p>Som et skoleprojekt lavede jeg sammen med min klassekammerat <a href="https://simonfromjakobsen.netlify.app" target="_blank" rel="noreferrer">Simon From Jakobsen</a> en webapp hvor man kunne skabe diskussionsrumme og sende beskeder til hinanden, samt med et registerings- og loginssystem.</p>
+            <p>Alt frontend er skabt af mig inkl. programmering i HTML, CSS og TypeScript, og alt backendarbejdet er lavet af <a href="https://simonfromjakobsen.netlify.app" target="_blank" rel="noreferrer">Simon From Jakobsen</a>.</p>
             <p>Du kan finde github siden for det projekt <a href="https://github.com/SimonFJ20/MessAnger/tree/frontend-v2" target="_blank" rel="noreferrer">her</a>.</p>
             <p>Ligesom denne side, er webappen også bygget til mobiler, dog skal man gå ind på /mobile for at få adgang til den, hvilket man ikke behøver for denne side.</p>
             <img class="project" src={messangerImage} alt="Et webapp der forestiller en chatting app. Den er sort og grøn i tema."></img>
@@ -43,15 +38,11 @@ function Portfolio() {
 
           <h2>FirmaCorp Site</h2>
             <p>For vores GF2 afsluttende eud eksamen bedes vi blandt andet lave en hjemmeside til et fiktivt firma.</p>
-            <p>For det lavede jeg sammen med min klassekammerat <a href="https://simonfj20.site" target="_blank" rel="noreferrer">Simon From Jakobsen</a> en hjemmeside med en startside med karrusel af forskellige produkter, produktliste og produktside, samt et Intra for at skabe og slette nye produkter og opdatere karrusellen.</p>
-            <p>Ligesom med MessAnger lavede jeg frontend mens <a href="https://simonfj20.site" target="_blank" rel="noreferrer">Simon</a> lavede backend, og hjalp hinanden hvor muligt.</p>
+            <p>For det lavede jeg sammen med min klassekammerat <a href="https://simonfromjakobsen.netlify.app" target="_blank" rel="noreferrer">Simon From Jakobsen</a> en hjemmeside med en startside med karrusel af forskellige produkter, produktliste og produktside, samt et Intra for at skabe og slette nye produkter og opdatere karrusellen.</p>
+            <p>Ligesom med MessAnger lavede jeg frontend mens Simon lavede backend, og hjalp hinanden hvor muligt.</p>
             <p>Det endte med at se ret godt ud, dog meget grundlæggende i design pga. tidsmæssige begrænsninger.</p>
-            <p>Startside</p>
-            <img src={splashImage} alt="Side med en karrusel af forskellige produkter."></img>
-            <p>Produktliste</p>
-            <img src={productListImage} alt="Side med en liste af forskellige produkter."></img>
-            <p>Produktside</p>
-            <img src={productPageImage} alt="Side med produktinformation, derunder en karrusel for produktbilleder, produkttitel og produktbeskrivelse."></img>
+            <img class="project" src={firmacorpImage} alt="Side med en karrusel af forskellige produkter."></img>
+            <p>Du kan se github siden for projektet <a href="https://github.com/SimonFJ20/firmacorp-site">her</a>.</p>
 
           <img class="icon" src={footerIcon} alt="Et blåt ikon der forestiller en portfolio."></img>
         </div>
