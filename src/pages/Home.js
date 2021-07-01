2594b0393ec6a32cee1ea7751d9726d187f48621
piet <42157469+camper0008@users.noreply.github.com>
2021-07-01 17:43:56 +0200

    added age script

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,27 @@
 import footerIcon from '../assets/images/icons/home-blue.svg';
+import React, { useState, useEffect } from 'react';
+
+const birthday = Date.UTC(2004, 4, 18);
 
 function Home() {
+    const [age, updateAge] = useState(0);
+    
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        updateAge(_ => {
+            const ageInMilliseconds = Date.now() - birthday;
+            return (ageInMilliseconds/(1000*60*60*24*365.25)).toFixed(9);
+        });
+      }, 1);
+      return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className="section footer">
             <h1>velkommen</h1>
-            <p>Hej, jeg er Pieter, en <span id="age">17</span> år gammel student og programmør.</p>
-            <p>Klik på linksene øverste oppe (eller til venstre, hvis du er på mobil), for at kunne få mere at vide om mig.</p>
+            <p>Hej, jeg er Pieter, en <span id="age">{age}</span> år gammel student og programmør.</p>
+            <p>Hvis du klikker på de links øverste oppe (eller til venstre, hvis du er på mobil), kan du få mere at vide om mig.</p>
             <img className="icon" src={footerIcon} alt="Et blåt ikon af et hus."></img>
         </div>
     );
