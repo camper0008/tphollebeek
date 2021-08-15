7932444a47fa617ea62b1abd8e68038f85445581
piet <42157469+camper0008@users.noreply.github.com>
2021-08-15 18:49:13 +0200

    added dark theme

---
+++ script.js
@@ -0,0 +1,45 @@
+// definitions
+const html = document.querySelector('html');
+const navbar = document.querySelector('#navbar');
+const darkThemeButton = document.querySelector('#toggle-dark-theme');
+const mqList = window.matchMedia('(prefers-color-scheme: dark)');
+
+let darkThemeEnabled = true;
+
+// pre-script
+darkThemeButton.setAttribute('style', 'display: initial;')
+
+// functions
+const updateNavbar = () => {
+    if (html.scrollTop !== 0) {
+        navbar.removeAttribute('class');
+    } else {
+        navbar.setAttribute('class', 'top-scrolled');
+    }
+}
+
+const toggleDarkTheme = () => {
+    darkThemeEnabled = !darkThemeEnabled;
+    
+    if (darkThemeEnabled) {
+        html.classList.add('dark-theme-enabled');
+        html.classList.remove('light-theme-enabled');
+        darkThemeButton.innerText = '🌞';
+    } else {
+        html.classList.add('light-theme-enabled');
+        html.classList.remove('dark-theme-enabled');
+        darkThemeButton.innerText = '🌙';
+    }
+}
+
+const onPreferenceChange = () => {
+    darkThemeEnabled = !mqList.matches;
+    toggleDarkTheme();
+}
+
+mqList.addEventListener('change', onPreferenceChange)
+darkThemeButton.addEventListener('click', toggleDarkTheme);
+window.addEventListener('scroll', updateNavbar);
+
+onPreferenceChange();
+updateNavbar();
