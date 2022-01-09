c66d32cadc824d1a4b978a974e163e481a016d87
piet <42157469+camper0008@users.noreply.github.com>
2022-01-10 00:18:16 +0100

    eleventy layouting

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,11 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
-    eleventyConfig.addPassthroughCopy('./src/images');
-    eleventyConfig.addPassthroughCopy('./src/fonts');
+    eleventyConfig.addPassthroughCopy({'src/images': 'images'});
+    eleventyConfig.addPassthroughCopy({'src/fonts': 'fonts'});
     // Return your Object options:
     return {
       dir: {
-        input: "src/website",
+        input: "src/layout",
         output: "public"
       }
     }
