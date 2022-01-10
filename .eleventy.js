6d74f3ad70534bb7609870aa5acd77d121a967ce
piet <42157469+camper0008@users.noreply.github.com>
2022-01-10 01:19:38 +0100

    date formatting

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,21 @@
+const padNumber = (num) => {
+    const stringified = num.toString();
+    if (stringified.length < 2)
+        return "0" + stringified;
+    else
+        return stringified;
+}
+
+const formatDate = (date) => {
+    return `${padNumber(date.getDate())}/${padNumber(date.getMonth()+1)}-${padNumber(date.getFullYear())}`
+}
+
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
     eleventyConfig.addPassthroughCopy({'src/images': 'images'});
     eleventyConfig.addPassthroughCopy({'src/fonts': 'fonts'});
+    eleventyConfig.addNunjucksFilter("formattedDate", function(value) { return formatDate(value) });
     // Return your Object options:
     return {
       dir: {
