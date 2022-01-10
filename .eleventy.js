4506c4f282a81f13ef382161ccb7c8a2dee4e40d
camper0008 <spetakel123@gmail.com>
2022-01-10 14:31:21 +0100

    syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,20 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 const months = ["Januar", "Februar", "Marts", "April", "Maj", "Juni", "Juli", "August", "September", "Oktober", "November", "December"];
 const getStringifiedMonth = (month) => months[month];
 const formatDate = (date) => `${date.getDate()}. ${getStringifiedMonth(date.getMonth())} ${date.getFullYear()}`;
 
-
 module.exports = function(eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
+    
     eleventyConfig.addPassthroughCopy({'src/images': 'images'});
     eleventyConfig.addPassthroughCopy({'src/fonts': 'fonts'});
     eleventyConfig.addPassthroughCopy({'src/favicon.ico': 'favicon.ico'});
+    
     eleventyConfig.addNunjucksFilter("formattedDate", function(value) { return formatDate(value) });
-    // Return your Object options:
+    
+    eleventyConfig.addPlugin(syntaxHighlight);
+   
     return {
       dir: {
         input: "src/layout",
