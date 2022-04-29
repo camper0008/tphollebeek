ac25b99e8a0f7cbbb74181ae652768f114d4a765
camper0008 <tphollebeek@gmail.com>
2022-04-29 11:14:33 +0200

    dynamic age counter

--- .eleventy.js
+++ .eleventy.js
@@ -6,25 +6,48 @@ const formatDate = (date) => `${date.getDate()}. ${getStringifiedMonth(date.getM
 
 const filterOffsiteAnchors = (html) => html.replaceAll("<a href=\"http", "<a target=\"_blank\" rel=\"noreferrer noopener\" href=\"http");
 
+const age = () => {
+    const birthday = Date.UTC(2004, 4, 18);
+    let ageInMilliseconds = Date.now() - birthday;
+    let ageInYears = (ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25)).toFixed(9);
+
+    return ageInYears;
+}
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
-    
-    eleventyConfig.addPassthroughCopy({'src/misc': 'misc'});
-    eleventyConfig.addPassthroughCopy({'src/images': 'images'});
-    eleventyConfig.addPassthroughCopy({'src/fonts': 'fonts'});
-    eleventyConfig.addPassthroughCopy({'src/favicon.ico': 'favicon.ico'});
-    eleventyConfig.addPassthroughCopy({'src/manifest.json': 'manifest.json'});
-    
-    eleventyConfig.addNunjucksFilter("formattedDate", formatDate );
-    eleventyConfig.addNunjucksFilter("lower", (value) => value.toLowerCase() );
-    eleventyConfig.addNunjucksFilter("filterOffsiteAnchors", filterOffsiteAnchors );
-    
+
+    eleventyConfig.addPassthroughCopy({
+        'src/misc': 'misc'
+    });
+    eleventyConfig.addPassthroughCopy({
+        'src/images': 'images'
+    });
+    eleventyConfig.addPassthroughCopy({
+        'src/js': 'js'
+    });
+    eleventyConfig.addPassthroughCopy({
+        'src/fonts': 'fonts'
+    });
+    eleventyConfig.addPassthroughCopy({
+        'src/favicon.ico': 'favicon.ico'
+    });
+    eleventyConfig.addPassthroughCopy({
+        'src/manifest.json': 'manifest.json'
+    });
+
+    eleventyConfig.addNunjucksFilter("age", age);
+    eleventyConfig.addNunjucksFilter("formattedDate", formatDate);
+    eleventyConfig.addNunjucksFilter("lower", (value) => value.toLowerCase());
+    eleventyConfig.addNunjucksFilter("filterOffsiteAnchors", filterOffsiteAnchors);
+
     eleventyConfig.addPlugin(syntaxHighlight);
-   
+
+
     return {
-      dir: {
-        input: "src/layout",
-        output: "public"
-      }
+        dir: {
+            input: "src/layout",
+            output: "public"
+        }
     }
-  };
+};
---
+++ src/js/age.js
@@ -0,0 +1,14 @@
+const age = () => {
+    const birthday = Date.UTC(2004, 4, 18);
+    let ageInMilliseconds = Date.now() - birthday;
+    let ageInYears = (ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25)).toFixed(9);
+
+    return ageInYears;
+}
+
+const element = document.querySelector("#age");
+const updateAge = () => {
+    element.textContent = age();
+};
+
+setInterval(updateAge, 1);
